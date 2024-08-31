c464f29118de44448677c20fd2fa844908a0794c
LEONARDO <leushu@gmail.com>
2024-08-31 16:29:06 -0300

    .

--- index.js
+++ index.js
@@ -9,19 +9,8 @@ const port = process.env.PORT || 3000;
 app.use(bodyParser.json());
 
 // cors - permitir solicitações de outros domínios
-const allowedOrigins = [
-  'http://localhost:5174',
-  'https://react-modern-nfyyzlcz1-leoalvarenga404s-projects.vercel.app/', // Adicione outras origens permitidas aqui
-];
-
 app.use(cors({
-  origin: (origin, callback) => {
-    if (allowedOrigins.includes(origin) || !origin) {
-      callback(null, true); // Permite a origem
-    } else {
-      callback(new Error('Not allowed by CORS')); // Rejeita a origem
-    }
-  }
+  origin: '*'
 }));
 
 app.use('/estudos', estudoRoutes);
