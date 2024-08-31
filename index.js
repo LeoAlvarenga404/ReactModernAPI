d41e087cbdb6050a11648bb85f42c8f240c42e9b
LEONARDO <leushu@gmail.com>
2024-08-31 16:32:32 -0300

    .

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ app.use(bodyParser.json());
 
 // cors - permitir solicitações de outros domínios
 app.use(cors({
-  origin: '*'
+  origin: 'http://localhost:5174', // Ajuste para o domínio do frontend
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Métodos permitidos
+  allowedHeaders: ['Content-Type', 'Authorization'] // Cabeçalhos permitidos
 }));
 
 app.use('/estudos', estudoRoutes);
